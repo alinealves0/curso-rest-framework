a5ad0b3b261a2f5fcaa0a6553f3055c0defc6729
aline <alinealves0@hotmail.com>
2024-09-18 18:24:06 -0300

    Primeiro serializer

---
+++ recipes/serializers.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers #type:ignore
+class RecipeSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=65)
+    description = serializers.CharField(max_length=165)
